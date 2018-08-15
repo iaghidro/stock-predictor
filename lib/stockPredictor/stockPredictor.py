997349b36be75c5607fada41881415a91cdf5f93
iaghidro <iaghidro@192.168.0.10>
2018-08-14 21:33:09 -0400

    added sample train and add ta functions, added tests

--- lib/stockPredictor/stockPredictor.py
+++ lib/stockPredictor/stockPredictor.py
@@ -3,8 +3,7 @@ import pandas as pd
 #from fastai.structured import *
 #from fastai.column_data import *
 #np.set_printoptions(threshold=50, edgeitems=20)
-
-#from ta import *
+from ta import *
 
 class StockPredictor:
     
@@ -17,12 +16,16 @@ class StockPredictor:
         self.train = self.df.head(trainRecordsCount)
 #        self.test.reset_index(inplace=True)
 #        self.train.reset_index(inplace=True)
-        print('StockPredictor::split_train_validation:: Train size: ' + str(len(self.train)) + ' Test size: ' + str(len(self.test)))
-    
-    def clean_data(self, df):
+        print('StockPredictor::split_train_validation:: Train size: ' + str(len(self.train)) + ' Test size: ' + str(len(self.test)))    
+
+    def sample_train(self, sampleSize):
+        self.train = self.df.tail(sampleSize)
+
+    def add_TA(self):
+        self.train = add_all_ta_features(self.train, "Open", "High", "Low", "Close", "Volume", fillna=True)
+        
+    def clean_train(self):
     #     df = df.dropna()
     #     df = df.replace(np.nan,df.mean())
-        df = df.replace([np.inf, -np.inf], np.nan)
-        df = df.fillna(method='bfill')
-        return df
-    
+        self.train = self.train.replace([np.inf, -np.inf], np.nan)
+        self.train = self.train.fillna(method='bfill')
--- lib/stockPredictor/test_stockPredictor.py
+++ lib/stockPredictor/test_stockPredictor.py
@@ -1,7 +1,7 @@
 #from fastai.structured import *
 #from fastai.column_data import *
 #np.set_printoptions(threshold=50, edgeitems=20)
-#from ta import *
+
 import pandas as pd
 import numpy as np
 
@@ -22,25 +22,41 @@ backfilledData = {
 testRecordsCount=2
 trainRecordsCount=3
 
-df = pd.DataFrame(data=data)
-predictor = StockPredictor(df)
+def createPredictor():
+    df = pd.DataFrame(data=data)
+    return StockPredictor(df)
     
 def test_constructor():
+    df = pd.DataFrame(data=data)
+    predictor = StockPredictor(df)
     assert predictor.df.equals(df)
 
 
 def test_split_train_validation():
+    df = pd.DataFrame(data=data)
+    predictor = createPredictor()
     predictor.split_train_validation(testRecordsCount, trainRecordsCount)
     assert predictor.train.equals(df.head(3))
     assert predictor.test.equals(df.tail(2))
     
     
-def test_clean_data():
-    filledDf = predictor.clean_data(pd.DataFrame(data=missingData))
-    expectedDf = predictor.clean_data(pd.DataFrame(data=backfilledData))
+#def test_add_TA():
+#    predictor.add_TA()
+    
+def test_sample_train():
+    df = pd.DataFrame(data=data)
+    predictor = createPredictor()
+    predictor.sample_train(3)
+    assert predictor.train.equals(df.tail(3))
+    
+def test_clean_train():
+    predictor = createPredictor()
+    predictor.train = pd.DataFrame(data=missingData)
+    predictor.clean_train()
+    expectedDf = pd.DataFrame(data=backfilledData)
     expectedDf.Close = expectedDf.Close.astype(float)
     expectedDf.Open = expectedDf.Open.astype(float)
-    assert filledDf.equals(expectedDf)
+    assert predictor.train.equals(expectedDf)
     
     
     
