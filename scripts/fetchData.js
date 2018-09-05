a03197777788beacea4f7ad9837c300c620a9a34
iaghidro <iaghidro@192.168.0.10>
2018-09-04 20:52:11 -0400

    added fetch data npm script, added fetch data function to model trainer

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -9,24 +9,18 @@ var trainModel = function () {
     console.log(`@@@@@@@@@ Start! @@@@@@@@@`);
 
     const trainer = new app.ModelTrainer({
-        amountChangePercentageThreshold: 0.5,
         timeDifferenceInMinutes: 30,
-        targetLookahead: 3,
-        recordsToRemove: 200, //todo: use percentBegin in splitting config
-        recordsToRemoveTestData: 2,
-        trainingName: "ethUsdCoinbase",
         bucketName: uploadDataS3BucketName,
         inputFileName: "historicalData.csv",
-        propertyFilter: config.featureSet.propertyFilters.RNN,
-        propertyMapping: config.featureSet.propertyMappings.coinAPI
     });
     const params = {
-        limit: 15000,
+        limit: 25000,
         symbolId: "COINBASE_SPOT_BTC_USD",
         startTime: new Date(Date.parse("2018-08-10T00:00:00.000Z")),
         endTime: null,//new Date(Date.parse("2018-02-24T00:00:00.000Z")),
         period: "1MIN",
-        fakeData: false
+        fakeData: false,
+        coinAPIKey: "3DA8C66B-8A17-47DB-8C1D-B765C2D1C0D1"
     };
     trainer.trainForML(params)
             .then((response) => {
