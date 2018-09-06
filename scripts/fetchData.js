cec2194640617ed2bb141382f734390593d4b05e
iaghidro <iaghidro@192.168.0.10>
2018-09-05 21:45:50 -0400

    fixed fetch data script

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -12,17 +12,17 @@ var trainModel = function () {
         timeDifferenceInMinutes: 30,
         bucketName: uploadDataS3BucketName,
         inputFileName: "historicalData.csv",
+        coinAPIKey: "3DA8C66B-8A17-47DB-8C1D-B765C2D1C0D1"
     });
     const params = {
         limit: 25000,
         symbolId: "COINBASE_SPOT_BTC_USD",
-        startTime: new Date(Date.parse("2018-08-10T00:00:00.000Z")),
+        startTime: new Date(Date.parse("2018-08-21T01:44:00.000Z")),
         endTime: null,//new Date(Date.parse("2018-02-24T00:00:00.000Z")),
-        period: "1MIN",
-        fakeData: false,
-        coinAPIKey: "3DA8C66B-8A17-47DB-8C1D-B765C2D1C0D1"
+        period: "1MIN"
     };
-    trainer.trainForML(params)
+
+    trainer.gatherCSVData(params)
             .then((response) => {
                 console.log(`finished training model`);
                 console.dir(response);
