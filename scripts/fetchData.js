e4c989f9cf0e3cceb22feba85dafaca4993554bc
iaghidro <iaghidro@192.168.0.10>
2018-09-08 14:48:49 -0400

    change fetch data script

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -15,9 +15,9 @@ var trainModel = function () {
         coinAPIKey: "3DA8C66B-8A17-47DB-8C1D-B765C2D1C0D1"
     });
     const params = {
-        limit: 25000,
-        symbolId: "COINBASE_SPOT_BTC_USD",
-        startTime: new Date(Date.parse("2018-08-21T01:44:00.000Z")),
+        limit: 50000,
+        symbolId: "COINBASE_SPOT_ETH_USD",
+        startTime: new Date(Date.parse("2018-07-21T01:00:00.000Z")),
         endTime: null,//new Date(Date.parse("2018-02-24T00:00:00.000Z")),
         period: "1MIN"
     };
