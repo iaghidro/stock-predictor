e85e3b0562c66d4e4612af618e623eb9de349b4c
iaghidro <iaghidro@192.168.0.10>
2018-09-09 17:40:07 -0400

    pylint formatting

--- lib/stockPredictor/stockPredictor.py
+++ lib/stockPredictor/stockPredictor.py
@@ -1,86 +1,95 @@
 import json as js
 import numpy as np
 import pandas as pd
+from ta import *
 #from fastai.structured import *
 #from fastai.column_data import *
-#np.set_printoptions(threshold=50, edgeitems=20)
-from ta import *
+
 
 class StockPredictor:
-    
+
     def __init__(self, df, index):
         self.df = df
-        self.index = index        
-                
+        self.index = index
+
     # ///////////////////////////////
     # /////// DATA CLEANING /////////
     # ///////////////////////////////
-    
+
     def sample_train(self, sampleSize):
         self.train = self.df.tail(sampleSize)
-        print('StockPredictor::sample_train:: Train size: ' + str(len(self.train)) + ' Original size: ' + str(len(self.df)))
-        
+        print('StockPredictor::sample_train:: Train size: ' +
+              str(len(self.train)) + ' Original size: ' + str(len(self.df)))
+
     def set_date_as_index(self):
         self.train[self.index] = pd.to_datetime(self.train[self.index])
 
     def set_date_as_index_unix(self):
-        self.train[self.index] = pd.to_datetime(self.train[self.index], unit='s')
-        
+        self.train[self.index] = pd.to_datetime(
+            self.train[self.index], unit='s')
+
     def split_train_validation(self, testRecordsCount, trainRecordsCount):
         self.test = self.df.tail(testRecordsCount)
         self.train = self.df.head(trainRecordsCount)
 #        self.test.reset_index(inplace=True)
 #        self.train.reset_index(inplace=True)
-        print('StockPredictor::split_train_validation:: Train size: ' + str(len(self.train)) + ' Test size: ' + str(len(self.test)))    
-        
+        print('StockPredictor::split_train_validation:: Train size: ' +
+              str(len(self.train)) + ' Test size: ' + str(len(self.test)))
+
     def normalize_train(self, volume, open, high, low, close):
         self.train = pd.DataFrame({
-            'Timestamp':self.train[self.index],
-            'Volume':self.train[volume],
-            'Open':self.train[open],
-            'High':self.train[high],
-            'Low':self.train[low],
-            'Close':self.train[close]
-        })[['Timestamp','Volume','Open','High','Low','Close']]
+            'Timestamp': self.train[self.index],
+            'Volume': self.train[volume],
+            'Open': self.train[open],
+            'High': self.train[high],
+            'Low': self.train[low],
+            'Close': self.train[close]
+        })[['Timestamp', 'Volume', 'Open', 'High', 'Low', 'Close']]
 
     def clean_train(self):
-    #     df = df.dropna()
-    #     df = df.replace(np.nan,df.mean())
+        #     df = df.dropna()
+        #     df = df.replace(np.nan,df.mean())
         self.train = self.train.replace([np.inf, -np.inf], np.nan)
         self.train = self.train.fillna(method='bfill')
-                        
+
     # ///////////////////////////////
     # //// FEATURE ENGINEERING //////
     # ///////////////////////////////
-    
+
     def add_ta(self):
-        self.train = add_all_ta_features(self.train, "Open", "High", "Low", "Close", "Volume", fillna=True)
-        
+        self.train = add_all_ta_features(
+            self.train, "Open", "High", "Low", "Close", "Volume", fillna=True)
+
     """ Set the target (dependent variable) by looking ahead in a certain time window and percent increase
-        to determine if the action should be a BUY or a SELL. BUY is true/1 SELL is false/0""" 
+        to determine if the action should be a BUY or a SELL. BUY is true/1 SELL is false/0"""
+
     def set_target(self, lookahead, percentIncrease):
-#        ,win_type='boxcar'
+        #        ,win_type='boxcar'
         max_in_lookahead_timeframe = self.train.Close \
-                                                .iloc[::-1] \
-                                                .rolling(window=lookahead,min_periods=1) \
-                                                .max() \
-                                                .iloc[::-1]
-        self.train['action'] = max_in_lookahead_timeframe > (percentIncrease * self.train.Close)
+            .iloc[::-1] \
+            .rolling(window=lookahead, min_periods=1) \
+            .max() \
+            .iloc[::-1]
+        self.train['action'] = max_in_lookahead_timeframe > (
+            percentIncrease * self.train.Close)
 #        self.train['max'] =max_in_lookahead_timeframe
         self.train.action = self.train.action.astype(int)
 
     def set_target_historical(self, lookahead, percentIncrease):
-        max_in_lookback_timeframe = self.train.Close.rolling(window=lookahead,min_periods=1).max()
-        self.train['action'] = max_in_lookback_timeframe > (percentIncrease * self.train.Close)
+        max_in_lookback_timeframe = self.train.Close.rolling(
+            window=lookahead, min_periods=1).max()
+        self.train['action'] = max_in_lookback_timeframe > (
+            percentIncrease * self.train.Close)
         self.train.action = self.train.action.astype(int)
-        buy_count = str(len(self.train[self.train.action==1]))
-        sell_count = str(len(self.train[self.train.action==0]))
-        print('StockPredictor::set_target_historical:: Buy count: ' + buy_count + ' Sell count: ' + sell_count)
-        
+        buy_count = str(len(self.train[self.train.action == 1]))
+        sell_count = str(len(self.train[self.train.action == 0]))
+        print('StockPredictor::set_target_historical:: Buy count: ' +
+              buy_count + ' Sell count: ' + sell_count)
+
     # ///////////////////////////////
     # ///////// EVALUATION //////////
     # ///////////////////////////////
-    
+
     def calculate_accuracy(self, df):
         successful_predictions = df.loc[df.action == df.predicted]
         total_accuracy = len(successful_predictions)/len(df)
@@ -89,18 +98,19 @@ class StockPredictor:
         successful_buy_predictions = successful_predictions.loc[successful_predictions.action == 1]
         successful_sell_predictions = successful_predictions.loc[successful_predictions.action == 0]
         buy_accuracy = len(successful_buy_predictions)/len(total_buy_actions)
-        sell_accuracy = len(successful_sell_predictions)/len(total_sell_actions)
-        f1Score = (buy_accuracy + sell_accuracy)/2;
+        sell_accuracy = (len(successful_sell_predictions) /
+                         len(total_sell_actions))
+        f1Score = (buy_accuracy + sell_accuracy)/2
         result = {
-            'F1Score': round(f1Score,3),
-            'totalAccuracy': round(total_accuracy,3),
-            'buyAccuracy': round(buy_accuracy,3),
-            'sellAccuracy': round(sell_accuracy,3),
+            'F1Score': round(f1Score, 3),
+            'totalAccuracy': round(total_accuracy, 3),
+            'buyAccuracy': round(buy_accuracy, 3),
+            'sellAccuracy': round(sell_accuracy, 3),
             'totalBuyActions': len(total_buy_actions),
             'successfulBuyPredictions': len(successful_buy_predictions)
         }
         return result
-            
+
     def calculate_net_profit(self, inputDf, startAmount):
         df = inputDf
         df['buyAmount'] = 0
@@ -108,34 +118,35 @@ class StockPredictor:
         totalBuys = 0
         totalSells = 0
         for index, row in df.iterrows():
-            prevBuyAmount = df.buyAmount.get(index -1, np.nan)
-            prevSellAmount = df.sellAmount.get(index -1, np.nan)
+            prevBuyAmount = df.buyAmount.get(index - 1, np.nan)
+            prevSellAmount = df.sellAmount.get(index - 1, np.nan)
     #         prevPredicted = df.predicted.get(index -1, np.nan)
             predicted = row.predicted
             if index == df.index[0]:
-                df.loc[index,'buyAmount'] = startAmount
+                df.loc[index, 'buyAmount'] = startAmount
             elif predicted == 1 and prevBuyAmount > 0:
                 # BUY
-                df.loc[index,'sellAmount'] = prevBuyAmount/row.Close
-                totalBuys +=1
+                df.loc[index, 'sellAmount'] = prevBuyAmount/row.Close
+                totalBuys += 1
             elif predicted == 1 and prevBuyAmount == 0:
-                df.loc[index,'sellAmount'] = prevSellAmount
+                df.loc[index, 'sellAmount'] = prevSellAmount
             elif predicted == 0 and prevSellAmount > 0:
-                # SELL             
-                df.loc[index,'buyAmount'] = prevSellAmount*row.Close
-                totalSells +=1
+                # SELL
+                df.loc[index, 'buyAmount'] = prevSellAmount*row.Close
+                totalSells += 1
             elif predicted == 0 and prevSellAmount == 0:
-                df.loc[index,'buyAmount'] = prevBuyAmount
+                df.loc[index, 'buyAmount'] = prevBuyAmount
             else:
                 # HOLD (not holding currently)
-                df.loc[index,'buyAmount'] = prevBuyAmount
-                df.loc[index,'sellAmount'] = prevSellAmount
-                
+                df.loc[index, 'buyAmount'] = prevBuyAmount
+                df.loc[index, 'sellAmount'] = prevSellAmount
+
         startClose = df.Close.iloc[0]
         endClose = df.Close.iloc[-1]
         endBuyAmount = df.buyAmount.iloc[-1]
         endSellAmount = df.sellAmount.iloc[-1]
-        endAmount = endBuyAmount if (endBuyAmount > 0) else (endSellAmount * endClose)
+        endAmount = endBuyAmount if (
+            endBuyAmount > 0) else (endSellAmount * endClose)
         buyAndHoldPercentIncrease = ((endClose - startClose)/startClose) * 100
         percentIncrease = ((endAmount - startAmount)/startAmount) * 100
         percentDifference = percentIncrease - buyAndHoldPercentIncrease
@@ -145,43 +156,43 @@ class StockPredictor:
             'endClose': endClose,
             'startAmount': startAmount,
             'endAmount': endAmount,
-            'buyAndHoldPercentIncrease':round(buyAndHoldPercentIncrease,3),
-            'percentIncrease':round(percentIncrease,3),
-            'percentDifference':round(percentDifference,3),
-            'totalTrades':totalBuys + totalSells
+            'buyAndHoldPercentIncrease': round(buyAndHoldPercentIncrease, 3),
+            'percentIncrease': round(percentIncrease, 3),
+            'percentDifference': round(percentDifference, 3),
+            'totalTrades': totalBuys + totalSells
         }
         self.net_profit_df = df
         self.result = result
-        print(js.dumps(result, sort_keys=False,indent=4, separators=(',', ': ')))
+        print(js.dumps(result, sort_keys=False, indent=4, separators=(',', ': ')))
 
     # ///////////////////////////////
     # /////////// UTIL //////////////
     # ///////////////////////////////
-    
+
     def save_to_feather(self):
         self.train.reset_index(inplace=True)
         self.train.to_feather(f'{PATH}train')
-    
+
     def read_from_feather(self):
         self.train = pd.read_feather(f'{PATH}train')
         # train.drop(self.index,1,inplace=True)
-    
+
     """ usage conflateTimeFrame(df, '5T') """
+
     def conflate_time_frame(self, df, timeFrame):
-        ohlc_dict = {                                                                                                             
-            'Open':'first',                                                                                                    
-            'High':'max',                                                                                                       
-            'High':'max',                                                                                                       
-            'Low':'min',                                                                                                        
-            'Close': 'last',                                                                                                    
+        ohlc_dict = {
+            'Open': 'first',
+            'High': 'max',
+            'Low': 'min',
+            'Close': 'last',
             'Volume': 'sum'
         }
         return df.resample(timeFrame).agg(ohlc_dict)
 
     def plot_profit(self, df):
         df.plot(
-            x='Timestamp', 
-            y=['Close', 'buyAmount'], 
+            x='Timestamp',
+            y=['Close', 'buyAmount'],
             style='o',
-            figsize=(10,5), 
+            figsize=(10, 5),
             grid=True)
--- lib/stockPredictor/test_stockPredictor.py
+++ lib/stockPredictor/test_stockPredictor.py
@@ -1,39 +1,39 @@
 #from fastai.structured import *
 #from fastai.column_data import *
-#np.set_printoptions(threshold=50, edgeitems=20)
-
 import numpy as np
 import pandas as pd
 from stockPredictor import StockPredictor
 
 trainData = {
-    'Close': [1, 1.006, 1.003, 1.005, 1.4], 
+    'Close': [1, 1.006, 1.003, 1.005, 1.4],
     'Open': [1.1, 1.2, 1.4, 1.8, 2],
     'Timestamp': [1325317920, 1325317980, 1325318040, 1325318100, 1325318160]
-} 
+}
 baseData = {
-    'Close': [1, 1.25, 1.5, 2, 2.25], 
+    'Close': [1, 1.25, 1.5, 2, 2.25],
     'Open': [1.1, 1.2, 1.4, 1.8, 2],
     'Timestamp': [1325317920, 1325317980, 1325318040, 1325318100, 1325318160],
     'action':    [1, 1, 0, 1, 0],
     'predicted': [0, 1, 0, 1, 1],
-} 
+}
 missingData = {
-    'Close': [1, np.nan, 6, 34, 6], 
+    'Close': [1, np.nan, 6, 34, 6],
     'Open': [np.nan, -np.inf, 78, 56, 7]
-}  
+}
 backfilledData = {
-    'Close': [1, 6, 6, 34, 6], 
+    'Close': [1, 6, 6, 34, 6],
     'Open': [78, 78, 78, 56, 7]
-}    
+}
 testRecordsCount = 2
 trainRecordsCount = 3
 index = 'Timestamp'
 
+
 def create_predictor(inputData):
     df = pd.DataFrame(data=inputData)
     return StockPredictor(df, index)
-    
+
+
 def test_constructor():
     df = pd.DataFrame(data=trainData)
     predictor = create_predictor(df)
@@ -44,18 +44,21 @@ def test_constructor():
 # /////// DATA CLEANING /////////
 # ///////////////////////////////
 
+
 def test_split_train_validation():
     df = pd.DataFrame(data=trainData)
     predictor = create_predictor(df)
     predictor.split_train_validation(testRecordsCount, trainRecordsCount)
     assert predictor.train.equals(df.head(3))
-    assert predictor.test.equals(df.tail(2))    
-    
+    assert predictor.test.equals(df.tail(2))
+
+
 def test_sample_train():
     df = pd.DataFrame(data=trainData)
     predictor = create_predictor(df)
     predictor.sample_train(3)
     assert predictor.train.equals(df.tail(3))
+
 
 def test_set_date_as_index_unix():
     predictor = create_predictor(pd.DataFrame(data=trainData))
@@ -67,7 +70,8 @@ def test_set_date_as_index_unix():
     timestamps = pd.DataFrame(data=timeData)
     timestamps.Timestamp = pd.to_datetime(timestamps.Timestamp)
     assert predictor.train.Timestamp.equals(timestamps.Timestamp)
-    
+
+
 def test_clean_train():
     predictor = create_predictor(trainData)
     predictor.train = pd.DataFrame(data=missingData)
@@ -76,23 +80,24 @@ def test_clean_train():
     expectedDf.Close = expectedDf.Close.astype(float)
     expectedDf.Open = expectedDf.Open.astype(float)
     assert predictor.train.equals(expectedDf)
-                    
+
 # ///////////////////////////////
 # //// FEATURE ENGINEERING //////
 # ///////////////////////////////
 
+
 def test_set_target():
     predictor = create_predictor(trainData)
     predictor.train = predictor.df
-    predictor.set_target(2,1.005)
+    predictor.set_target(2, 1.005)
     action_data = {
-        'action': [1,0,0,1,0]
+        'action': [1, 0, 0, 1, 0]
     }
     actions = pd.DataFrame(data=action_data)
     actions.action = actions.action.astype(int)
     assert predictor.train.action.equals(actions.action)
-        
-    
+
+
 # ///////////////////////////////
 # ///////// EVALUATION //////////
 # ///////////////////////////////
@@ -110,7 +115,8 @@ def test_calculate_accuracy():
         'successfulBuyPredictions': 2
     }
     assert result == expected
-    
+
+
 def test_calculate_net_profit():
     df = pd.DataFrame(data=baseData)
     predictor = create_predictor(baseData)
@@ -122,25 +128,25 @@ def test_calculate_net_profit():
         'startAmount': 100,
         'endAmount': 135,
         'buyAndHoldPercentIncrease': 125,
-        'percentIncrease':35,
+        'percentIncrease': 35,
         'percentDifference': -90,
         'totalTrades': 3
     }
     assert predictor.result == expected
-    
+
 # ///////////////////////////////
 # /////////// UTIL //////////////
 # ///////////////////////////////
-    
+
+
 def test_save_to_feather():
     predictor = create_predictor(baseData)
     predictor.train = predictor.df.tail(3)
 #    predictor.save_to_feather('stockPredictor/')
 #    indexDf = pd.DataFrame(data=[0,1,2])
 #    assert predictor.train.index.equals(indexDf)
-    
-    
-    
+
+
 #    print('******')
 #    print(filledDf.to_string())
 #    print('******')
