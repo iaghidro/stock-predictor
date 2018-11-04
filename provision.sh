68fc2db146ab888c55a3b81ad421be1ff1488d62
iaghidro <iaghidro@192.168.0.10>
2018-11-04 17:59:38 -0500

    install jupyter

--- provision.sh
+++ provision.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# INSTALL DEPENDENCIES
+#todo: create machine learning image and move dependencies there
+echo "INSTALL PYTHON DEPENDENCIES";
+apt-get update
+apt install -y ipython3
+
+pip3 install pytest
+pip3 install ta
+pip3 install pandas
+pip3 install jupyter
+export PATH=$PATH:~/.local/bin/
+
 echo "INSTALL DEPENDENCIES";
 npm i;
 
